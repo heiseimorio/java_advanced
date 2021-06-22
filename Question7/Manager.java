2366d7fd9d46865177e8513c7aff456ccca223e4
末金和浩 <k.suekane@digitalvision.co.jp>
2021-06-22 14:02:56 +0900

    ManagerクラスにEmployeeクラスを継承。

--- Question7/Manager.java
+++ Question7/Manager.java
@@ -1,7 +1,9 @@
 package Question7;
 
+import Question5.Employee;
 import Question6.Management;
 
-public class Manager implements Management {
+public class Manager extends Employee implements Management {
+
   public void manage() {};
 }
