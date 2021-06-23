d54b0ed4eb2a3b38121b1736cbfcfd9ca359fea1
末金和浩 <k.suekane@digitalvision.co.jp>
2021-06-23 15:05:02 +0900

    report.txtを読込、Scannerで入力された値と共にreport3.txtに出力処理の実装

---
+++ Question11/ReadWriteFile.java
@@ -0,0 +1,70 @@
+package Question11;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ReadWriteFile {
+  public static void main(String[] args) {
+    // ファイル名を指定
+    File file = new File("report.txt");
+
+    // 上記ファイル名のフルパスを取得
+    String filePath = file.getAbsolutePath();
+
+    Scanner sc = new Scanner(System.in);
+    try(BufferedReader br = new BufferedReader(new FileReader(filePath)))
+      {
+        String line;
+        List<String> data = new ArrayList<>();
+
+        while ((line = br.readLine()) != null) {
+          data.add(line);
+        }
+
+        String inputContet = sc.nextLine();
+        String blank = " 	";
+        String workContent = blank + inputContet;
+
+        String inputTime = sc.nextLine();
+        String time = blank + inputTime;
+        sc.close();
+
+        data.set(3, workContent);
+        data.set(6, time);
+
+        // ファイル名を指定
+        file = new File("report3.txt");
+
+        // 上記ファイル名のフルパスを取得
+        filePath = file.getAbsolutePath();
+
+        // FileWriterクラスのオブジェクトを生成する
+        FileWriter fileWriter = new FileWriter(filePath);
+
+        // PrintWriterクラスのオブジェクトを生成する
+        PrintWriter printWriter = new PrintWriter(new BufferedWriter(fileWriter));
+
+        // PrintWriterクラスのprintlnメソッドを使ってファイルに書き込む
+        for(String text: data) {
+          printWriter.println(text);
+        }
+
+        // 書き込み対象のファイルを閉じてリソースを開放する
+        printWriter.close();
+      }
+      catch (IOException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+      }
+
+  }
+}
